8d159f7d1f6f80667bb17286fb1a95fbff329469
HRS295RICKY <71895974+HRS295RICKY@users.noreply.github.com>
2021-07-17 19:05:24 +0100

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    
+    text("R E V B A I L A", 100,50)
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+    text("A T A D E S B A", 700,150)
+    text("Hint: Use for multiplayer game !!", 700,170)
+    
+    text("C U T N I F O N", 100,370)
+    text("Hint: Use for a better presentation !!", 100,390)
+
+}
